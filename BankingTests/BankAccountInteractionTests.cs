db9dfbf34ce96f63889d62af64678db2c4bd4e54
Christopher Spradlin <github@crspradlin.org>
2021-03-17 14:48:08 -0400

    manual stub of BankAccountInteractionTests

---
+++ BankingTests/BankAccountInteractionTests.cs
@@ -0,0 +1,48 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountInteractionTests
+    {
+        [Fact]
+        public void DepositUsesTheBonusCalculator()
+        {
+            var stubbedBonusCaculator = new StubbedBonusCalculator();
+            var account = new BankAccount(stubbedBonusCaculator);
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 10M;
+
+            stubbedBonusCaculator.AmountToReturn = 42;
+            stubbedBonusCaculator.ExpectedAmountOfDeposit = amountToDeposit;
+            stubbedBonusCaculator.ExpectedBalance = openingBalance;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(openingBalance + amountToDeposit + 42, account.GetBalance());
+        }
+    }
+
+    public class StubbedBonusCalculator : ICanCalculateBankAccountBonuses
+    {
+        public decimal ExpectedBalance;
+        public decimal ExpectedAmountOfDeposit;
+        public decimal AmountToReturn;
+        
+        public decimal For(decimal balance, decimal amountToDeposit)
+        {
+            if(balance == ExpectedBalance && amountToDeposit == ExpectedAmountOfDeposit)
+            {
+                return AmountToReturn;
+            } else
+            {
+                return -10; // something dumb, to make sure everything is working right
+            }
+        }
+    }
+}
