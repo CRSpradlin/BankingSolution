0256b767534da193e9424182bfe876ddef026365
Christopher Spradlin <github@crspradlin.org>
2021-03-17 10:34:03 -0400

    added flag

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -2,9 +2,11 @@
 
 namespace BankingDomain
 {
+    public enum AccountTypes { Standard, Gold }
     public class BankAccount
     {
         private decimal _balance = 5000; // Class Variable AKA: "Field"
+        public AccountTypes AccountType = AccountTypes.Standard;
 
         public BankAccount()
         {
@@ -17,7 +19,12 @@ namespace BankingDomain
 
         public void Deposit(decimal amountToDeposit)
         {
-            _balance += amountToDeposit;
+            decimal bonus = 0;
+            if (AccountType == AccountTypes.Gold)
+            {
+                bonus = amountToDeposit * .10M;
+            }
+            _balance += amountToDeposit + bonus;
         }
 
         public void Withdraw(decimal amountToWithdraw)
---
+++ BankingTests/GoldCustomerTests.cs
@@ -0,0 +1,28 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldCustomerTests
+    {
+
+        [Theory]
+        [InlineData(100,10)]
+        public void GetABonusOnDeposites(decimal amountToDeposit, decimal expectedBonus)
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            account.AccountType = AccountTypes.Gold;
+
+            account.Deposit(amountToDeposit);
+
+            var expected = openingBalance + amountToDeposit + expectedBonus;
+            Assert.Equal(expected, account.GetBalance());
+        }
+    }
+}
